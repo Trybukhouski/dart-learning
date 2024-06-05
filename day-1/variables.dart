bc1c3fd90c9af8c715092bb4b794d92f5564cb6e
Trybukhouski <trybukhouskij@gmail.com>
2024-06-06 02:13:37 +0300

    implement function with variables

---
+++ day-1/variables.dart
@@ -0,0 +1,9 @@
+void main() {
+  String name = 'Ruslan';
+  int age = 36;
+  double temperature = 36.6;
+  bool isStudent = false;
+  print('name: $name');
+  print('age: $age');
+  print('temperature: $temperature');
+}
