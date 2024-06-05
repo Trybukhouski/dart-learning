3b7ee8f047f5bce22fd125a11ebd5198f2e0e668
Trybukhouski <trybukhouskij@gmail.com>
2024-06-06 02:53:58 +0300

    implement function with loops

---
+++ day-1/loops.dart
@@ -0,0 +1,19 @@
+void main() {
+  for(int index = 1; index < 10; index++) {
+    print(index);
+  }
+
+  List<int> numbers = [1,2,3,4,5];
+
+  for(int number in numbers) {
+    print(number);
+  }
+
+  int index = 1;
+
+  while(index< 10) {
+    print(index);
+    index++;
+  }
+
+}
