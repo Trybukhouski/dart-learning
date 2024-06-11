51c9236e7877e120b16e0931712e315b85626b41
Trybukhouski <trybukhouskij@gmail.com>
2024-06-11 22:57:49 +0300

    feat: implement task invert-values

---
+++ day-2/invert-values.dart
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/5899dc03bc95b1bf1b0000ad
+
+List<int> invert(List<int> arr) {
+  List<int> invertArr = [];
+  arr.forEach((el) => invertArr.add(-el));
+  return invertArr;
+}
+
+void main() {
+  print(invert([2,4,-3,-2,4,-3]));
+}
