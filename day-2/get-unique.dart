36fb253ce66515be8960f5ac6499f176d05a2fd6
Trybukhouski <trybukhouskij@gmail.com>
2024-06-11 23:12:35 +0300

    feat: implement task get-unique

---
+++ day-2/get-unique.dart
@@ -0,0 +1,10 @@
+List<int> getUnique(List<int> integers) {
+  Set<int> unique = Set();
+  integers.forEach((integer) => unique.add(integer)
+  );
+  return unique.toList();
+}
+
+void main() {
+  print(getUnique([1,1,2,3,4,4,6,7]));
+}
