ccd108c9fab0ff58e48a7e8995076083420bccfc
Trybukhouski <trybukhouskij@gmail.com>
2024-06-12 15:29:35 +0300

    feat: add solution for task create-future-obj in TS

---
+++ day-2/create-future-obj.ts
@@ -0,0 +1,14 @@
+function sum(): number {
+    return 10 + 5;
+}
+
+function main() {
+    const future: Promise<number> = new Promise(resolve => {
+        resolve(sum())
+    });
+    future.then((value) => console.log(`async task ${value}`))
+
+    console.log('sync task')
+}
+
+main();
